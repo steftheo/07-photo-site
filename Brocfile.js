e6cef5bc9256c5a70aa4e5002d50c9dead0a35e6
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-03-11 11:35:03 -0600

    got cssgram working

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,7 @@
 const Merge = require('broccoli-merge-trees');
 const Sass = require('broccoli-sass');
 const LiveReload = require('broccoli-inject-livereload');
+const Autoprefixer = require('broccoli-autoprefixer');
 
 const public = new LiveReload('public');
 
@@ -14,4 +15,4 @@ const stylePaths = [
 
 const styles = new Sass(stylePaths, 'app.scss', 'app.css');
 
-module.exports = new Merge([public, styles]);
+module.exports = new Merge([public, new Autoprefixer(styles)]);
